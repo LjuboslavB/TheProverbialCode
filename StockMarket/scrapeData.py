10cfa377abf6fc5ea3c71e4bd03da0c612abf393
melo-gonzo <carmelojsg@gmail.com>
2018-10-15 23:21:17 -0700

    Added a function and class. Can start data processing now. YAS

--- StockMarket/scrapeData.py
+++ StockMarket/scrapeData.py
@@ -11,6 +11,7 @@ Main entry point to script is download_quotes
 import re
 import time
 import requests
+import pandas as pd
 
 
 def split_crumb_store(v):
@@ -20,8 +21,6 @@ def split_crumb_store(v):
 
 
 def find_crumb_store(lines):
-    # Looking for
-    # ,"CrumbStore":{"crumb":"9q.A4D1c.b9
     for l in lines:
         if re.findall(r'CrumbStore', l):
             return l
@@ -38,10 +37,6 @@ def get_page_data(symbol):
     url = "https://finance.yahoo.com/quote/%s/?p=%s" % (symbol, symbol)
     r = requests.get(url)
     cookie = get_cookie_value(r)
-
-    # Code to replace possible \u002F value
-    # ,"CrumbStore":{"crumb":"FWP\u002F5EFll3U"
-    # FWP\u002F5EFll3U
     lines = r.content.decode('unicode-escape').strip(). replace('}', '\n')
     return cookie, lines.split('\n')
 
@@ -63,28 +58,24 @@ def get_data(symbol, start_date, end_date, cookie, crumb):
 
 
 def get_now_epoch():
-    # @see https://www.linuxquestions.org/questions/programming-9/python-datetime-to-epoch-4175520007/#post5244109
     return int(time.time())
 
 
 def download_quotes(symbols):
-
     if len(symbols) > 1:
         for i in range(1, len(symbols)):
             symbol = symbols[i]
             print("--------------------------------------------------")
             print("Downloading %s to %s.csv" % (symbol, symbol))
-            # download_quotes(symbol)
             print("--------------------------------------------------")
             start_date = 0
             end_date = get_now_epoch()
             cookie, crumb = get_cookie_crumb(symbol)
             get_data(symbol, start_date, end_date, cookie, crumb)
     else:
-        symbol = symbols
+        symbol = symbols[0]
         print("--------------------------------------------------")
         print("Downloading %s to %s.csv" % (symbol, symbol))
-        # download_quotes(symbol)
         print("--------------------------------------------------")
         start_date = 0
         end_date = get_now_epoch()
@@ -92,14 +83,38 @@ def download_quotes(symbols):
         get_data(symbol, start_date, end_date, cookie, crumb)
 
 
+class StockClass:
+    def __init__(self, symbol, filename):
+        try:
+            self.data = pd.read_csv(filename)
+            self.name = [symbol]
+            print('adding data for stock %s' % symbol)
+        except Exception:
+            self.data = []
+            self.name = 'No File'
+            print('file: %s.csv' % symbols[i], 'does not exist')
+
+
+def parse_csv(symbols):
+    stock = []
+    print(len(symbols))
+    for i in range(len(symbols)):
+        try:
+            filename = 'C:/Users/carme/Desktop/C_D/Projects/Python/Stock/TheProverbialCode/StockMarket' \
+                       '/CSVFiles/' + '%s.csv' % (symbols[i])
+            stock.append(StockClass(symbols[i], filename))
+        except Exception:
+            print('file: %s.csv' % symbols[i], 'does not exist')
+    return stock
+
+
 tickers = open('SP500.txt', 'r')
 tickers = tickers.read()
 tickers = tickers.split('\n')
-# tickers = ['APD']
-download_quotes(tickers)
-
-
-
+# download_quotes(tickers)
+# tickers = ['ZION']
+stocks = parse_csv(tickers)
+print(stocks[10].name)
 
 
 
@@ -139,3 +154,59 @@ download_quotes(tickers)
 # crumble_regex = r'CrumbStore":{"crumb":"(.*?)"}'
 # cookie_regex = r'Set-Cookie: (.*?); '
 # quote_link = 'https://query1.finance.yahoo.com/v7/finance/download/{}?period1={}&period2={}&interval=1d&events=history&crumb={}'
+
+#
+# class StockClass:
+#     def __init__(self, symbol, filename):
+#         try:
+#             self.data = pd.read_csv(filename)
+#             self.name = [symbol]
+#             print('adding data')
+#         except Exception:
+#             self.data=[]
+#             self.name='No File'
+#             print('file: %s.csv' % symbols[i], 'does not exist')
+#
+#
+# def parse_csv(symbols):
+#     for i in range(len(symbols)):
+#         try:
+#             filename = 'C:/Users/carme/Desktop/C_D/Projects/Python/Stock/TheProverbialCode/StockMarket' \
+#                        '/CSVFiles/' + '%s.csv' % (symbols[i])
+#         except Exception:
+#             filename='Nothing'
+#             print('file: %s.csv' % symbols[i], 'does not exist')
+#         stock = StockClass(symbols[i], filename)
+#     return stock
+
+# class StockClass:
+#
+#     def __init__(self, symbols):
+#         self.max = len(symbols)
+#
+#     def __iter__(self):
+#         self.n = 0
+#         return self
+#
+#     def __next__(self, symbols):
+#         if self.n <= self.max:
+#             try:
+#                 filename = 'C:/Users/carme/Desktop/C_D/Projects/Python/Stock/TheProverbialCode' \
+#                            '/StockMarket' \
+#                            '/CSVFiles/' + '%s.csv' % (symbols[self.n])
+#                 self.data[self.n] = pd.read_csv(filename)
+#                 self.name[self.n] = [symbols[self.n]]
+#                 print('adding data')
+#             except Exception:
+#                 self.data[self.n] = []
+#                 self.name[self.n] = 'No File'
+#                 print('file: %s.csv' % symbols[self.n], 'does not exist')
+#             self.n += 1
+#         else:
+#             raise StopIteration
+#         return self
+#
+#
+# def parse_csv(symbols):
+#     stock_data = StockClass(symbols)
+#     return stock_data
